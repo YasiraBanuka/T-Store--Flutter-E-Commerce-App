343a39cefe4c702444f74d2cd9bb1c9d5d7f9ff2
IT21378270 <it21378270@my.sliit.lk>
2023-12-04 14:49:17 +0530

    completed onboarding screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
 /// -- Use this class to setup themes, initial bindings, any animations and much more using Material Widget
@@ -7,10 +9,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
