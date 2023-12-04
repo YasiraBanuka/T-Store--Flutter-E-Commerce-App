fcbca4ed4e53dc9fb857cb6e1d1874817a93c563
IT21378270 <it21378270@my.sliit.lk>
2023-12-04 17:49:31 +0530

    login screen created

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:t_store/common/styles/spacing_styles.dart';
+import 'package:t_store/common/widgets/login_signup/form_divider.dart';
+import 'package:t_store/common/widgets/login_signup/social_buttons.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_form.dart';
+import 'package:t_store/features/authentication/screens/login/widgets/login_header.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              /// Logo, Title, & Sub-title
+              const TLoginHeader(),
+
+              /// Form
+              const TLoginForm(),
+
+              /// Divider
+              TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Footer
+              const TSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
---
+++ lib/features/authentication/screens/login/widgets/login_header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class TLoginHeader extends StatelessWidget {
+  const TLoginHeader({
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+          height: 150,
+          image: AssetImage(dark ? TImages.lightAppLogo : TImages.darkAppLogo),
+        ),
+        Text(TTexts.loginTitle,
+          style: Theme.of(context).textTheme.headlineMedium,),
+        const SizedBox(height: TSizes.sm),
+        Text(TTexts.loginSubTitle,
+          style: Theme.of(context).textTheme.bodyMedium,),
+      ],
+    );
+  }
+}
