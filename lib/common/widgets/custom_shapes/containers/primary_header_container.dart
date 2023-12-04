10fdb8824cabe17ec3b6867b2e1b4739b9213de8
IT21378270 <it21378270@my.sliit.lk>
2023-12-05 00:13:09 +0530

    designed custom container & clip path

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:t_store/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:t_store/utils/constants/colors.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: const EdgeInsets.all(0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              /// --- Background Custom Shapes
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1)),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularContainer(
+                    backgroundColor: TColors.textWhite.withOpacity(0.1)),
+              ),
+              child,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+
