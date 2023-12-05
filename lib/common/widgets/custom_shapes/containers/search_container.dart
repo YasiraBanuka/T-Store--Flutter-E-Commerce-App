2437c0ef8162398c6bbefc9c84adb5bff72f9b84
IT21378270 <it21378270@my.sliit.lk>
2023-12-05 14:09:47 +0530

    modified search bar with onTap()

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -12,44 +12,49 @@ class TSearchContainer extends StatelessWidget {
     this.icon = Iconsax.search_normal,
     this.showBackground = true,
     this.showBorder = true,
+    this.onTap,
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: TSizes.defaultSpace,
-      ),
-      child: Container(
-        width: TDeviceUtils.getScreenWidth(context),
-        padding: const EdgeInsets.all(TSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground
-              ? dark
-                  ? TColors.dark
-                  : TColors.light
-              : Colors.transparent,
-          borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: TColors.grey) : null,
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: TSizes.defaultSpace,
         ),
-        child: Row(
-          children: [
-            Icon(
-              icon,
-              color: TColors.darkerGrey,
-            ),
-            const SizedBox(width: TSizes.spaceBtwItems),
-            Text(
-              text,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          ],
+        child: Container(
+          width: TDeviceUtils.getScreenWidth(context),
+          padding: const EdgeInsets.all(TSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                    ? TColors.dark
+                    : TColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(TSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: TColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(
+                icon,
+                color: TColors.darkerGrey,
+              ),
+              const SizedBox(width: TSizes.spaceBtwItems),
+              Text(
+                text,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+            ],
+          ),
         ),
       ),
     );
