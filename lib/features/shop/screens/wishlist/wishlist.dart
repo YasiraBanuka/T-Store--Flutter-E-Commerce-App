8e1a10d9964c70095853191aeadea3f11dc117c5
IT21378270 <it21378270@my.sliit.lk>
2023-12-14 12:25:32 +0530

    designed wishlist screen

--- lib/features/shop/screens/wishlist/wishlist.dart
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -26,7 +26,7 @@ class FavouriteScreen extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
             children: [
               TGridLayout(itemCount: 6, itemBuilder: (_, index) => const TProductCardVertical()),
