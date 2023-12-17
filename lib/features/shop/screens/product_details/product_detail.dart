205ebc883283fee18b65e89fa76475564a96d068
IT21378270 <it21378270@my.sliit.lk>
2023-12-18 00:14:37 +0530

    designed up to price, title, stock, & brand section of product detail page

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:t_store/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:t_store/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
@@ -29,6 +30,7 @@ class ProductDetailScreen extends StatelessWidget {
                   TRatingAndShare(),
 
                   /// -- Price, Title, Stock, & Brand
+                  TProductMetaData(),
 
                   /// --- Attributes
                   /// Checkout Button
--- lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,10 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:t_store/common/widgets/images/t_circular_image.dart';
+import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/common/widgets/texts/product_title_text.dart';
+import 'package:t_store/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/enums.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 class TProductMetaData extends StatelessWidget {
   const TProductMetaData({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final darkMode = THelperFunctions.isDarkMode(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        /// Price & Sale Price
+        Row(
+          children: [
+            /// -- Sale Tag
+            TRoundedContainer(
+              radius: TSizes.sm,
+              backgroundColor: TColors.secondary.withOpacity(0.8),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: TSizes.sm, vertical: TSizes.xs),
+              child: Text(
+                '25%',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .apply(color: TColors.black),
+              ),
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems),
+
+            /// -- Price
+            Text(
+              '\$250',
+              style: Theme.of(context)
+                  .textTheme
+                  .titleSmall!
+                  .apply(decoration: TextDecoration.lineThrough),
+            ),
+            const SizedBox(width: TSizes.spaceBtwItems),
+            const TProductPriceText(price: '175', isLarge: true),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+        /// Title
+        const TProductTitleText(title: 'Green Nike Sports Shirt'),
+        const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+        /// Stock Status
+        Row(
+          children: [
+            const TProductTitleText(title: 'Status'),
+            const SizedBox(width: TSizes.spaceBtwItems),
+            Text('In Stock', style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ),
+        const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+        /// Brand
+        Row(
+          children: [
+            TCircularImage(
+              image: TImages.shoeIcon,
+              width: 32,
+              height: 32,
+              overlayColor: darkMode ? TColors.white : TColors.black,
+            ),
+            const TBrandTitleWithVerifiedIcon(
+              title: 'Nike',
+              brandTextSize: TextSizes.medium,
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
