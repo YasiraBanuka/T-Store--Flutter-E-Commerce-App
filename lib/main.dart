5fdd4fe6468616f6074f77cb72960126c0a7211e
IT21378270 <it21378270@my.sliit.lk>
2023-12-20 12:48:56 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/app.dart';
+import 'package:t_store/firebase_options.dart';
 
 /// ----- Entry point of Flutter App --------
-void main() {
-
+Future<void> main() async {
   // Todo: Add Widgets Binding
   // Todo: Init Local Storage
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
   // Todo: Initialize Authentication
 
   runApp(const App());
 }
-
